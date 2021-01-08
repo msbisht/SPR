c18bbb077d32509f275d64561b3afb01f08fd869
manjits <manjitsingh.bisht@gardnerdenver.com>
2021-01-08 21:36:21 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SPR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
